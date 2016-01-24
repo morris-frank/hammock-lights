62b61b951f23c63290e2976783c9a6aa68079bcb
Tukkin Moriarty <mrtukkin@gmail.com>
2016-01-24 22:27:38 +0100

    Added distance

--- main.go
+++ main.go
@@ -7,22 +7,56 @@ import (
         "time"
 )
 
-type Pin uint8
-
 //The used pins
 const (
   pinNW = rpio.Pin(0)
   pinNE = rpio.Pin(2)
   pinSW = rpio.Pin(3)
   pinSE = rpio.Pin(4)
+
+  pinTrigger = rpio.Pin(7)
+  pinEcho = rpio.Pin(8)
 )
 
-func carousel() {
+func setup() {
+   pinNW.Output()
+   pinNE.Output()
+   pinSW.Output()
+   pinSE.Output()
 
+   pinTrigger.Output()
+   pinEcho.Input()
+}
+
+func distance() float64 {
+  pinTrigger.High()
+
+  //Wait for 10µs
+  time.Sleep(time.Microsecond * 10)
+  pinTrigger.Low()
+
+  LaunchTime := time.Now()
+  ReturnTime := time.Now()
+
+  for pinEcho.Read() == 0 {
+    LaunchTime = time.Now()
+  }
+
+  for pinEcho.Read() == 1 {
+    ReturnTime = time.Now()
+  }
+
+  ShotTime := ReturnTime.Sub(LaunchTime)
+
+  //Multiply the elapsed time with the speed of sound and divide by 2 as
+  //the shot has to travel back
+  distance := (ShotTime.Seconds() * 34300) / 2
+
+  return distance
 }
 
 func main() {
-	fmt.Printf("Starting hammock-lights...\n")
+	fmt.Printf("Starting hammock-light s...\n")
 
   // Open and map memory to access gpio, check for errors
   if err := rpio.Open(); err != nil {
@@ -32,10 +66,7 @@ func main() {
 
    defer rpio.Close()
 
-   pinNW.Output()
-   pinNE.Output()
-   pinSW.Output()
-   pinSE.Output()
+   setup()
 
    for i := 0; i < 20; i++ {
      pinNW.Toggle()
